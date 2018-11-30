8f77e703390b9d8e5bf794e7f2f50620a0e96979
Bryan Lee <bryanlee21@live.com>
2018-11-29 21:29:16 -0800

    Roll a ball tutorial finished

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //shows up in editor as an editable property since public
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //fixed update for when dealing with forces
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    private void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 12)
+        {
+            winText.text = "YOU WIN!";
+        }
+
+    }
+}
+
---
+++ Assets/Scenes/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // not dealing with forces so can just use update
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+	}
+}
